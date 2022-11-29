d60efb47e681112139622f0a8d9d0ae91d72fe65
AslanUrdiev <aurdiev@mail.ru>
2022-11-29 21:39:10 +0300

    first commit
    Создаем структуру и объект с по которому будет идти сравнение

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('span').textContent;
+const result = {};
+
+const staticData = {
+  'х': 'хром',
+  'н': 'никель',
+  'т': 'титан',
+  'д': 'медь',
+  'г': 'марганец',
+  'м': 'молибден',
+  'в': 'вольфрам',
+  'с': 'кремний',
+  'к': 'кобальт'
+}
